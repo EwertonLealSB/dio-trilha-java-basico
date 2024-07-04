e189f1a00a17f5595ac930786cc80e6a694671a8
Ewerton <ewertonbrito14@gmail.com>
2024-07-03 23:30:08 -0300

    Operadores - 1: Parte Final

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -65,7 +65,75 @@ public class Operadores {
         a = 6;
         b = 6; 
         String valor = (a==b) ? "verdadeiro" : "false";
-        System.out.println(valor);
+        System.out.printf("\n",valor);
 
-    }
+        //Relacional 
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2; 
+        System.out.printf("\n",simNao);
+        
+        if(numero1 > numero2)
+        	System.out.print("Numero 1 maior que numero 2\n");
+
+        if(numero1 < numero2)
+        	System.out.print("Numero 1 menor que numero 2\n");
+
+        if(numero1 >= numero2)
+        	System.out.print("Numero 1 maior ou igual que numero 2\n");
+
+        if(numero1 <= numero2)
+        	System.out.print("Numero 1 menor ou igual que numero 2\n\n");
+
+        //// ComparacaoAvancada
+        if(numero1 != numero2)
+        	System.out.print("Numero 1 é diferente de numero 2\n");
+
+        String nome1 = "JAVA";
+        String nome2 = "JAVA";
+
+        System.out.println(nome1 == nome2); //true
+    
+        String nome3 = new String("JAVA");
+        System.out.println(nome1 == nome3); //false
+    
+        String nome4 = nome3;
+        System.out.println(nome3 == nome4); //true
+
+        int numero3 = 130;
+        int numero4 = 130;
+        System.out.println(numero3 == numero4); //true
+        
+        Integer numero5 = 130;
+        Integer numero6 = 130;
+    
+        System.out.println(numero5 == numero6); //false
+        
+        // A razão pela qual o resultado é false, é devido o Java tratar os valores
+        // Como objetos a partir de agora.
+        // Qual a solução ?
+        // Quando queremos comparar objetos, usamos o método equals
+        
+         System.out.println(numero5.equals(numero6));
+
+
+        
+        //Lógico: && Operador Lógico "E", || Operador Lógico "OU"
+        boolean condicao1=true;
+        boolean condicao2=false;
+        
+        /* Aqui estamos utilizando o operador lógico E para fazer a união de duas 
+        expressões. 
+        É como se estivesse escrito:
+         "Se Condicao1 e Condicao2 forem verdadeiras, executar código"
+        */
+        
+        if(condicao1 && condicao2)
+            System.out.print("Os dois valores precisam ser verdadeiros");;
+        
+        //Se condicao1 OU condicao2 for verdadeira, executar código.
+        if(condicao1 || condicao2)
+            System.out.print("Um dos valores precisa ser verdadeiro\n");
+    }   
 }
