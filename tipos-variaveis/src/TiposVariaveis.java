4095e9541c0407e92897568391d0bc1fb296ec14
Ewerton <ewertonbrito14@gmail.com>
2024-07-03 23:40:37 -0300

    Criação de tabela - tipos de Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        //Tabela tipo Variaveis
+        /*FAMILIA   -   TIPO PRIMITIVO  -   WRAPPER CLASS   -   TAMANHO
+         *LÓGICO    -   boolean         -   boolean         -   1 bit
+         * LITERAIS -   char            -   Character       -   1 byte
+         *          -     -             -   String          -   1 byte/cada
+         * INTEIROS -   byte            -   Byte            -   1 byte
+         *          -   short           -   Short           -   2 bytes
+         *          -   int             -   Integer         -   4 bytes
+         *          -   long            -   Long            -   8 bytes
+         * REAIS    -   float           -   Float           -   4 bytes 
+         *          -   double          -   Double          -   8 bytes
+        */
+
+
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
@@ -10,6 +24,7 @@ public class TiposVariaveis {
         System.out.print(numero);
 
         final double VALOR_DE_PI = 3.14;
+
 
     }
 }
